eab1e300b9bb1fb86239b601961de4450bf74d3e
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2024-08-23 13:25:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -539,7 +539,7 @@ resource "azurerm_public_ip" "spokevm-pip" {
   name                = "spokevm-pip"
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
-  allocation_method = "Dynamic"
+  allocation_method = "Static"
   timeouts {
     create = "2h"
     read = "2h"
